067dede5d1db45ae91e0e0b722266273ea406c48
jou_2015MAC <jou66jou@gmail.com>
2019-06-17 16:31:50 +0800

    update some code

--- models/helper/router_register.go
+++ models/helper/router_register.go
@@ -1,10 +1,8 @@
 package helper
 
 import (
-	"fmt"
 	"net/http"
 
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 )
 
@@ -31,30 +29,4 @@ func AddLoginRouters(router mux.Router, routes []Route) mux.Router {
 	}
 	return router
 
-}
-
-// JWT decode
-func TokenMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		tokenStr := r.Header.Get("authorization")
-		if tokenStr == "" {
-			ResponseWithJson(w, http.StatusUnauthorized,
-				Response{Code: http.StatusUnauthorized, Msg: "authorized is \"\""})
-		} else {
-			token, _ := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
-				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-					ResponseWithJson(w, http.StatusUnauthorized,
-						Response{Code: http.StatusUnauthorized, Msg: "not ok authorized"})
-					return nil, fmt.Errorf("not authorization")
-				}
-				return []byte(secretWord), nil
-			})
-			if !token.Valid {
-				ResponseWithJson(w, http.StatusUnauthorized,
-					Response{Code: http.StatusUnauthorized, Msg: "!token.Valid authorized"})
-			} else {
-				next.ServeHTTP(w, r)
-			}
-		}
-	})
 }
