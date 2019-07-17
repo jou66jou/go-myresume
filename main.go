5e3f513e6f507236c00f3292143fb83e7cab2613
jase_2015MAC <jou66jou@gmail.com>
2019-07-17 14:42:31 +0800

    test

--- main.go
+++ main.go
@@ -1,6 +1,40 @@
 //隨手測試用
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+)
 
+type A struct {
+	Name string `json:"name"`
+}
+
+type B struct {
+	Age int `json:"age"`
+}
+
+type hub struct {
+	C1 chan interface{}
+	C2 chan interface{}
+}
+
+func do(c chan interface{}) {
+	for k := range c {
+		json, _ := json.Marshal(k)
+		fmt.Println(string(json))
+	}
+}
+
+func main() {
+	hub := &hub{make(chan interface{}), make(chan interface{})}
+	go func() {
+		hub.C1 <- A{"jaseHuang"}
+		hub.C2 <- B{25}
+	}()
+
+	go do(hub.C1)
+	go do(hub.C2)
+	for {
+	}
 }
